5f1878439cbe3c9995e75a0f0c5ef53d3badad07
Roman <74274722+Ro-JA@users.noreply.github.com>
2023-03-13 22:26:19 +0900

    add remember

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,23 +1,25 @@
 package com.example.tiptime
 
 import android.os.Bundle
-import android.text.Layout.Alignment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.tiptime.ui.theme.TipTimeTheme
+import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -39,13 +41,24 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun TipTimeScreen() {
     Column(modifier = Modifier.padding(32.dp),
-    verticalArrangement = Arrangement.spacedBy(8.dp)){
+    verticalArrangement = Arrangement.spacedBy(8.dp))
+    {
         Text(
             text = stringResource(R.string.calculate_tip),
             fontSize = 24.sp,
-
+            modifier = Modifier.align(Alignment.CenterHorizontally)
         )
+        Spacer(modifier = Modifier.height(16.dp))
+        EditNumberField()
+
     }
+
+}
+
+@Composable
+fun EditNumberField() {
+    var amountInput by remember { mutableStateOf("") }
+    TextField(value = amountInput, onValueChange = {amountInput = it} )
 
 }
 
